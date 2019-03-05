4715faf1a7dc8888e02b9e98044d88bd6e088d63
wuzhenzhen <kgd.zhen@gmail.com>
2019-03-05 11:18:45 +0800

    readme

--- app/src/main/java/com/github/zlog/MyApplication.java
+++ app/src/main/java/com/github/zlog/MyApplication.java
@@ -19,8 +19,9 @@ public class MyApplication extends Application {
         builder.logSavePath("/sdcard/EBSB_SDCardLog"); //设置Log 保存的文件夹
         builder.logCatLogLevel(ZLog.SHOW_ALL_LOG); //控制台输出日志等级
         builder.fileLogLevel(ZLog.SHOW_ALL_LOG);   //保存文件日志等级
-        builder.fileOutFormat(ZLog.LOG_OUTPUT_FORMAT_3);
+//        builder.fileOutFormat(ZLog.LOG_OUTPUT_FORMAT_3);
         ZLog.initialize(this, builder.build());
+        ZLog.iii("------MyApplication------");
 
     }
 }
