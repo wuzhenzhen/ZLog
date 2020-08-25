1b42120ad1a8f7cf11fab6e4ef23c19caf82fb5a
wuzhenzhen <kgd.zhen@gmail.com>
2020-08-25 13:52:11 +0800

    AscIITools

--- zbase/src/main/java/com/github/zbase/tools/AscIITools.java
+++ zbase/src/main/java/com/github/zbase/tools/AscIITools.java
@@ -3,6 +3,7 @@ package com.github.zbase.tools;
 import android.util.Log;
 
 import java.io.ByteArrayOutputStream;
+import java.io.UnsupportedEncodingException;
 
 /**
  * Created by wzz on 2017/03/01.
@@ -663,4 +664,97 @@ public class AscIITools {
         return result;
     }
 
+
+    // 反转 ，用来处理，大端，小端
+    public static String toSmallEnd(String hex){
+        //0001C200
+        String smallEnd = "";
+        if(hex.length()%2 == 0){
+            for(int i=0; i<hex.length()/2; i++){
+                smallEnd = hex.substring(i*2,i*2+2) + smallEnd;
+            }
+        }
+        return smallEnd;
+    }
+
+    /**
+     * UTF-8编码 转换为对应的 汉字
+     *
+     * URLEncoder.encode("上海", "UTF-8") ---> %E4%B8%8A%E6%B5%B7
+     * URLDecoder.decode("%E4%B8%8A%E6%B5%B7", "UTF-8") --> 上 海
+     *
+     * convertUTF8ToString("E4B88AE6B5B7")
+     * E4B88AE6B5B7 --> 上海
+     *
+     * @param hexStr
+     * @return
+     */
+    public static String convertUTF8ToString(String hexStr) {
+        if (hexStr == null || hexStr.equals("")) {
+            return null;
+        }
+
+        try {
+            hexStr = hexStr.toUpperCase();
+
+            int total = hexStr.length() / 2;
+            int pos = 0;
+
+            byte[] buffer = new byte[total];
+            for (int i = 0; i < total; i++) {
+
+                int start = i * 2;
+
+                buffer[i] = (byte) Integer.parseInt(
+                        hexStr.substring(start, start + 2), 16);
+                pos++;
+            }
+
+            return new String(buffer, 0, pos, "UTF-8");
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return hexStr;
+    }
+
+    /**
+     *  将汉字转为UTF8编码的串
+     * 将文件名中的汉字转为UTF8编码的串,以便下载时能正确显示另存的文件名.
+     * convertStringToUTF8("上海")
+     * 上海 --> E4B88AE6B5B7
+     * @param s	原串
+     * @return
+     */
+    public static String convertStringToUTF8(String s) {
+        if (s == null || s.equals("")) {
+            return null;
+        }
+        StringBuffer sb = new StringBuffer();
+        try {
+            char c;
+            for (int i = 0; i < s.length(); i++) {
+                c = s.charAt(i);
+                if (c >= 0 && c <= 255) {
+                    sb.append(c);
+                } else {
+                    byte[] b;
+
+                    b = Character.toString(c).getBytes("utf-8");
+
+                    for (int j = 0; j < b.length; j++) {
+                        int k = b[j];
+                        if (k < 0)
+                            k += 256;
+                        sb.append(Integer.toHexString(k).toUpperCase());
+                        // sb.append("%" +Integer.toHexString(k).toUpperCase());
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+        return sb.toString();
+    }
 }
